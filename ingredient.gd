5bded5d76196e20976fd2e9afcf0e9b0c44c3b38
ZedArray <hafidzuddin.ra@gmail.com>
2024-07-25 16:54:56 +0700

    drag and drop in item_alchemy done

--- ingredient.gd
+++ ingredient.gd
@@ -1,18 +1,30 @@
 extends RigidBody2D
 
+signal clicked
+
 var lifted = false
 
 func _ready():
 	get_viewport().get_mouse_position()
 
-func _on_input_event(event):
-	if event is InputEventMouseButton and event.pressed:
-		lifted = true
+func _physics_process(delta):
+	if lifted:
+		global_transform.origin = get_global_mouse_position()
+		#position = get_viewport().get_mouse_position()
 
-func _unhandled_input(event):
-	if event is InputEventMouseButton and not event.pressed:
-		lifted = false
-	if lifted and event is InputEventMouseMotion:
-		position += event.relative
+func _on_input_event(x, event, y):
+	#lifted = event.is_pressed()
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			linear_velocity = Vector2(0, 0)
+			lifted = not lifted
+			print(lifted)
 
-
+#func _unhandled_input(event):
+	#if event is InputEventMouseButton and not event.pressed:
+		#pass
+		##print("test")
+	#if lifted and event is InputEventMouseMotion:
+		#pass
+		##print("test2")
+		##position = get_viewport().get_mouse_position()
